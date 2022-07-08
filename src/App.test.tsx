6631c1347796833509f5a41908398c33fcb2bea7
balovbohdan <balovbohdan@gmail.com>
2022-07-08 10:01:09 +0300

    examples

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,43 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+
+const addEventListener = jest.spyOn(document, 'addEventListener');
+
+jest.mock('uuid', () => ({
+  v4: () => 'uuid_v4'
+}));
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+test('it renders', () => {
+  const { container } = render(<App />);
+
+  expect(container).toMatchSnapshot();
+});
+
+test('it increments (fireEvent)', () => {
+  const { debug } = render(<App />);
+
+  const button = screen.getByRole('button', { name: /increment/i });
+
+  userEvent.click(button);
+  userEvent.click(button);
+  userEvent.click(button);
+
+  // eslint-disable-next-line testing-library/no-debugging-utils
+  debug();
+});
+
+test('id adds document click listener', async () => {
+  render(<App />);
+
+  await waitFor(() => {
+    expect(addEventListener).toBeCalledTimes(2);
+  });
+});
 
 test('renders learn react link', () => {
   render(<App />);
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,85 @@
-import React from 'react';
+import { useState, createContext, useContext, useEffect, useCallback } from 'react';
+import { v4 } from 'uuid';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/*
+App
+  Header1
+    Header2
+      Paragraph1
+        Paragraph2
+*/
+
+const CounterContext = createContext<{
+  value: number,
+  incerement: () => void
+}>({
+  value: 0,
+  incerement() {}
+});
+
+const useCounterContext = () =>
+  useContext(CounterContext);
+
+function Paragraph() {
+  const counterContext = useCounterContext();
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        Edit <code>src/App.tsx</code> and save to reload.
+        Count: {counterContext.value}
+        <button onClick={() => {
+          counterContext.incerement();
+        }}>Increment</button>
+      </p>
+  )
+}
+
+function Header() {
+  return (
+    <header className="App-header">
+      <Paragraph />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+}
+
+function App() {
+  const [count, setCount] = useState<number>(0);
+
+  const handleDocumentClick = useCallback(() => {
+    console.log('document clicked!');
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('click', handleDocumentClick);
+
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, [handleDocumentClick]);
+
+  return (
+    <CounterContext.Provider value={{
+      value: count,
+      incerement() {
+        setCount((currentCount) => currentCount + 1);
+      }
+    }}>
+      <div className="App">
+        <Header />
+        {v4()}
+      </div>
+    </CounterContext.Provider>
   );
 }
 
